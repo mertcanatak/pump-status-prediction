3e9e883c9597c229a5c63bb91d54c88313b46f91
Mert Can Atak <115237534+mertcanatak@users.noreply.github.com>
2024-09-03 16:42:34 +0300

    solved.py
    the blank page error in streamlit cloud was caused by long page name LOL

--- stlit/Mainpage.py
+++ stlit/Mainpage.py
@@ -48,7 +48,7 @@ if page == "Overview":
 
 elif page == "Statistics":
     st.title("Statistics")
-    model_url = "https://raw.githubusercontent.com/mertcanatak/pump-status-prediction/main/stlit/2_Model.py"
+    model_url = "https://raw.githubusercontent.com/mertcanatak/pump-status-prediction/main/stlit/1_Statistics.py"
     response = requests.get(model_url)
     if response.status_code == 200:
         exec(response.text)  
@@ -57,7 +57,7 @@ elif page == "Statistics":
 
 elif page == "Model":
     st.title("LSTM Model Prediction")
-    stats_url = "https://raw.githubusercontent.com/mertcanatak/pump-status-prediction/main/stlit/1_Statistics.py"
+    stats_url = "https://raw.githubusercontent.com/mertcanatak/pump-status-prediction/main/stlit/2_Model.py"
     response = requests.get(stats_url)
     if response.status_code == 200:
         exec(response.text)
