8d367db29ffffa37a55a96403707106479c6a8e8
Mert Can Atak <115237534+mertcanatak@users.noreply.github.com>
2024-09-04 11:28:35 +0300

    Update and rename def_full.py to pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -8,7 +8,8 @@ from tensorflow.keras.optimizers import Adam
 
 
 class LSTMPipeline:
-    def __init__(self, data_path, model_path="https://raw.githubusercontent.com/mertcanatak/water-pump-status-prediction/main/lstm_model.keras"):
+    def __init__(self, data_path,
+                 model_path="C:/Users/Mert Can/PycharmProjects/pythonProject/lstm_model.keras"):
         self.data_path = data_path
         self.model_path = model_path
         self.sensor_columns = ['sensor_00', 'sensor_01', 'sensor_02', 'sensor_03', 'sensor_04',
@@ -21,11 +22,9 @@ class LSTMPipeline:
         self.model = None
 
     def load_data(self):
-        """Veriyi dosyadan okur ve DataFrame olarak kaydeder."""
         self.df = pd.read_csv(self.data_path)
 
     def preprocess_data(self):
-        """Veriyi işleyerek model için hazır hale getirir."""
         # machine_status mapping
         status_mapping = {"NORMAL": 0, "RECOVERING": 1, "BROKEN": 1}
         self.df['machine_status'] = self.df['machine_status'].map(status_mapping)
@@ -44,17 +43,14 @@ class LSTMPipeline:
         self.y = self.df['shifted_machine_status'].values
 
     def split_data(self):
-        """Veriyi eğitim, test ve M_test setlerine böler."""
         X_train, X_temp, y_train, y_temp = train_test_split(self.X, self.y, test_size=0.3, random_state=42)
         X_test, X_M_test, y_test, y_M_test = train_test_split(X_temp, y_temp, test_size=2 / 3, random_state=42)
         return X_train, X_test, X_M_test, y_train, y_test, y_M_test
 
     def load_model(self):
-        """Kaydedilmiş modeli belirtilen dosya yolundan yükler."""
         self.model = load_model(self.model_path)
 
     def evaluate_model(self, X_test, y_test, X_M_test, y_M_test):
-        """Modeli test ve M_test setlerinde değerlendirir ve sonuçları döndürür."""
         y_pred = (self.model.predict(X_test) > 0.5).astype(int)
         test_accuracy = accuracy_score(y_test, y_pred)
         test_report = classification_report(y_test, y_pred)
@@ -66,18 +62,3 @@ class LSTMPipeline:
         cm = confusion_matrix(y_M_test, y_pred_M_test)
 
         return test_accuracy, test_report, M_test_accuracy, M_test_report, cm
-
-
-# https://www.kaggle.com/datasets/nphantawee/pump-sensor-data/data
-pipeline = LSTMPipeline("pythonProject/sensor.csv")
-pipeline.load_data()
-pipeline.preprocess_data()
-X_train, X_test, X_M_test, y_train, y_test, y_M_test = pipeline.split_data()
-
-pipeline.load_model()
-
-test_accuracy, test_report, M_test_accuracy, M_test_report, cm = pipeline.evaluate_model(X_test, y_test, X_M_test, y_M_test)
-print(f"Test Set Accuracy (Loaded Model): {test_accuracy:.4f}")
-print(f"M_test Set Accuracy (Loaded Model): {M_test_accuracy:.4f}")
-print("Confusion Matrix (Loaded Model):")
-print(cm)
