b8c57a906a7cede246447872fd0adb63e37af329
Mert Can Atak <mertcanatk@gmail.com>
2024-09-04 11:35:49 +0300

    pipeline mainrun

---
+++ main.py
@@ -0,0 +1,24 @@
+from pipeline import LSTMPipeline
+
+def main():
+    pipeline = LSTMPipeline("C:/Users/Mert Can/PycharmProjects/pythonProject/sensor.csv")
+
+    pipeline.load_data()
+    pipeline.preprocess_data()
+
+    X_train, X_test, X_M_test, y_train, y_test, y_M_test = pipeline.split_data()
+
+    pipeline.load_model()
+
+    test_accuracy, test_report, M_test_accuracy, M_test_report, cm = pipeline.evaluate_model(X_test, y_test, X_M_test, y_M_test)
+
+    print(f"Test Set Accuracy: {test_accuracy:.4f}")
+    print(test_report)
+    print(f"M_test Set Accuracy: {M_test_accuracy:.4f}")
+    print(M_test_report)
+    print("Confusion Matrix:")
+    print(cm)
+
+
+if __name__ == "__main__":
+    main()
